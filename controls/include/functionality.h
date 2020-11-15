e4defd8acc7d337a2180eb4909ba03e218b78fba
pigcoder3 <pigcoder3@gmail.com>
2020-11-15 09:28:58 -0500

    Migrated motor functionality to new file so it can be used with autonomous as well

---
+++ controls/include/functionality.h
@@ -0,0 +1,41 @@
+/*----------------------------------------------------------------------------------*/
+/*                                                                                  */
+/*    Module:       functionality.h                                                 */
+/*    Author:       Sean Johnson, Richard Wang, Luke Wittbrodt (Firehawks Robotics) */
+/*    Created:      Thurs Oct 22 2020                                               */
+/*    Description:  Change Up Motor Movement Functionality Definitions              */
+/*                                                                                  */
+/*----------------------------------------------------------------------------------*/
+
+#include "vex.h"
+
+using namespace vex;
+
+const int SPEED = 200; //rpm
+
+const int MAX_AXIS_VALUE = 127;
+//const int MIN_AXIS_VALUE = -127;
+
+//If not driver mode, then autonomous mode
+extern bool driverMode; 
+
+/*
+ * Moves stuff
+*/
+void movement(double x, double y, double turnvalue);
+
+/*
+ * Moves the liftLeft and liftRight motors according to desired direction
+ * 0 = In
+ * 1 = Out
+*/
+void intake(int inOrOut);
+
+/*
+ * Moves the liftLeft and liftRight motors according to desired direction
+ * 0 = Up
+ * 1 = Down
+*/
+void lift(int upOrDown);
+
+void modeToggled();
---
+++ controls/src/autonomous.cpp
@@ -0,0 +1,20 @@
+/*----------------------------------------------------------------------------------*/
+/*                                                                                  */
+/*    Module:       autonomous.cpp                                                  */
+/*    Author:       Sean Johnson, Richard Wang, Luke Wittbrodt (Firehawks Robotics) */
+/*    Created:      Sun Nov 15 2020                                                 */
+/*    Description:  Change Up Autonomous                                            */
+/*                                                                                  */
+/*----------------------------------------------------------------------------------*/
+
+#include "vex.h"
+
+using namespace vex;
+
+#include "functionality.h"
+
+// Procedural
+void autonomous() {
+
+  
+}
