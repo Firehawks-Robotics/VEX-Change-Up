0eaf924d235aa39270fd775c5baa6fdf871a7931
pigcoder3 <pigcoder3@gmail.com>
2020-11-22 15:46:08 -0500

    Updated the prototype to use more realistic pause timing so itcan be used later for initial testing

--- AutoLeftHome/src/autonomous.cpp
+++ AutoLeftHome/src/autonomous.cpp
@@ -33,17 +33,19 @@ timeUnits ms = timeUnits::msec; //Just to save a few keystrokes
  * - All pausing times will need to be updated
 */
 
+//1000 milliseconds = 1 second
 void pause(int milliseconds) { wait(20, ms); }
 
+//At prototype, this should take 7 seconds (lmao, yeah right)
 void auton() {
     //Move NE to get in front of goal
     movement(MAX_AXIS_VALUE, SIDE*MAX_AXIS_VALUE, 0);
-    pause(20);
+    pause(500);
 
     //Turn around
     movement(0, 0, SIDE*MAX_AXIS_VALUE);
 
-    pause(20);
+    pause(200);
 
     movement(0, 0, 0); //Stop turning
 
@@ -51,58 +53,58 @@ void auton() {
     movement(MAX_AXIS_VALUE, 0, 0);
     intake(IN);
 
-    pause(20);
+    pause(200);
 
     movement(0, 0, 0); //Stop moving
 
-    pause(20); //Make this just enough to get them in the intake, but not the lift
+    pause(200); //Make this just enough to get them in the intake, but not the lift
 
     intake(STOPINTAKE);
 
     //Turn Around and put preload ball in goal
     movement(0, 0, SIDE*MAX_AXIS_VALUE);
 
-    pause(20);
+    pause(200);
 
     movement(0, 0, 0); //Stop turning
 
     lift(UP); //Now put the preload ball in the goal
 
-    pause(20);
+    pause(200);
 
     lift(STOPLIFT);
 
     //Go score one ball in the middle goal
     movement(0, 0, -1*SIDE*MAX_AXIS_VALUE); //Fix angle
 
-    pause(20);
+    pause(500);
 
     movement(-1*SIDE*MAX_AXIS_VALUE, 0, 0); //Stop turning and start moving
 
-    pause(20);
+    pause(1500);
 
     movement(0, 0, 0); //Stop once reached the middle goal
 
     lift(UP); //Put the ball in the goal
 
-    pause(20); //stop before we put the last ball in there (we dont want that)
+    pause(200); //stop before we put the last ball in there (we dont want that)
 
     lift(STOPLIFT);
 
     //Go to the last goal on the home row
     movement(-1*SIDE*MAX_AXIS_VALUE, MAX_AXIS_VALUE, 0); //Give wide berth to other robot so it doesnt get in the way
     
-    pause(20);
+    pause(1500);
 
     movement(-1*SIDE*MAX_AXIS_VALUE, -MAX_AXIS_VALUE, 0); //Turn around half way
 
-    pause(20);
+    pause(1500);
 
     movement(0, 0, 0); //Stop in front of the goal
 
     lift(UP); //Put the last ball in the last goal
 
-    pause(20);
+    pause(200);
 
     lift(STOPLIFT); //Stop everything
 
