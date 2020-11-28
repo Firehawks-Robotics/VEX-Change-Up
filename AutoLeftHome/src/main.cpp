b588954b38aeabd9f820289e6366f89f54f5f1d2
pigcoder3 <pigcoder3@gmail.com>
2020-11-28 14:53:19 -0500

    Tick length (20 ms) is now defined by a constant

--- AutoLeftHome/src/main.cpp
+++ AutoLeftHome/src/main.cpp
@@ -78,9 +78,9 @@ int main() {
 
     //Movement is handled by an infinite while loop to ensure that the movement gets updated like it should
     //Sometimes the axis.changed event does not happen even if the axis value does change. Thus, our current solution.
-    while(1) { //Each iteration of this loop is one tick (so each tick is about 20 ms)
+    while(1) { //Each iteration of this loop is one tick
         movement(omnidirectionalX.value(), omnidirectionalY.value(), turning.value());
-        wait(20, timeUnits::msec); //Use less battery this way
+        wait(TICK_LENGTH, timeUnits::msec); //Use less battery this way
         debugMenuController(); //Debug screen is updated every tick
     }
 }
