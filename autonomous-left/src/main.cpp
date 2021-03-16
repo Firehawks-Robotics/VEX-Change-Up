504efcb72ead47bc7d69cb06a53b83d9a51a4fab
pigcoder3 <pigcoder3@gmail.com>
2021-03-16 16:54:27 -0400

    Revert "Revert "Fixed drive train stuff not going appropriate distances""
    This reverts commit 53e171f74f2e2e085cffd3b91ea40fd62abbf362.

--- autonomous-left/src/main.cpp
+++ autonomous-left/src/main.cpp
@@ -54,9 +54,9 @@ void pre_auton() {
     train.setDriveVelocity(DRIVE_TRAIN_SPEED, rpm);
 }
 
-constexpr double levelOnePower = .2;
-constexpr double levelTwoPower = .4;
-constexpr double levelThreePower = .7;
+constexpr double levelOnePower = 0.4;
+constexpr double levelTwoPower = 0.8;
+constexpr double levelThreePower = 1.4;
 
 void updateSpeedPower(double newPower) {
     percentOfMaxSpeed = newPower;
